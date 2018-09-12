46ae059814978ea0be9e425c50aff0d459abaea9
Tasha Mitchell <tamitchell14@gmail.com>
2018-09-12 11:55:07 -0400

    about import sass

--- weatherapp/src/Weather.js
+++ weatherapp/src/Weather.js
@@ -2,17 +2,16 @@ import React from 'react'
 
 export default function Weather (props) {
     let obj = props.data
-    if(obj === undefined || Object.getOwnPropertyNames(obj).length === 0) {
-        console.log(obj)
+    if(!obj || !Object.getOwnPropertyNames(obj).length) {
         return <div>Enter in a city, zipcode, or address</div>
     } else {
-        console.log(obj)
+        let temp = Math.round(obj.currently.temperature)
         return<div>
             <p>{obj.minutely.summary}</p>
-            <p>{obj.currently.temperature}</p>
-            <p>{obj.currently.humidity}</p>
-            <p>{obj.currently.windSpeed}</p>
-            <p>{obj.currently.uvIndex}</p>
+            <p>Temp: {temp}</p>
+            <p>Humidity: {obj.currently.humidity}</p>
+            <p>WindSpeed: {obj.currently.windSpeed}</p>
+            <p>UV : {obj.currently.uvIndex}</p>
             <p>{obj.currently.precipProbability}</p>
             </div>
             
