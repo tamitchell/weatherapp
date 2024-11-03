cefb657d420a755bb4a4f53dac303a23207100f8
Tasha <24355829+tamitchell@users.noreply.github.com>
2024-11-03 09:37:26 -0500

    fix: updated text size for safari and ios devices

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,13 @@ export const metadata: Metadata = {
   title: 'First Watch Weather',
   description:
     "A React/NextJS Based Application that uses OpenWeather's web API to gather weather data and Geocodio's API to render weather conditions based on user's desired location",
+    viewport: {
+      width: 'device-width',
+      initialScale: 1,
+      maximumScale: 1,
+      userScalable: false,
+      viewportFit: 'cover',
+    },
 };
 
 const APILoaderWrapper = dynamic(
--- src/components/GooglePlacesPicker.tsx
+++ src/components/GooglePlacesPicker.tsx
@@ -11,8 +11,14 @@ export default memo(function GooglePlacesPicker({
     <div className="custom-place-picker">
       <PlacePicker
         className={clsx(
-          'w-full h-full border-gray-300 text-gray-700 bg-gray-100 custom-place-picker'
+          'w-full h-full border-gray-300 text-gray-700 bg-gray-100 custom-place-picker',
+          "text-base leading-normal", 
+
         )}
+        style={{
+          fontSize: '16px', //prevent autozoom on ios/safari devices
+          touchAction: 'manipulation'
+        }}
         placeholder="Enter a location name or address"
         onPlaceChange={handlePlaceChange}
       />
