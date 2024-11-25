861ab9abaae56dc5c7dcba5b0a98a50daa6b22a5
Tasha <24355829+tamitchell@users.noreply.github.com>
2024-11-24 19:29:15 -0500

    chore: corrected build script

--- src/components/OutfitRecommendation/OutfitRecommendation.tsx
+++ src/components/OutfitRecommendation/OutfitRecommendation.tsx
@@ -9,7 +9,6 @@ import { useMemo } from 'react';
 import getPrecipitationForecast from 'src/util/calculateChanceOfPrecip/getPrecipitationForecast';
 import WordReveal from '../WordReveal/WordReveal';
 
-
 export default function OutfitRecommendation() {
   const { units } = useWeather();
   const { data: location } = useGeolocationQuery();
